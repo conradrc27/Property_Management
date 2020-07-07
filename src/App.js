0bef93a93ed0aa3266e07f0545d26971058ab6e2
conradoRC <conrado27cym@gmail.com>
2020-07-07 13:27:00 -0500

    User registration form

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import ListaInmobiliaria from "./components/views/ListaInmobiliaria";
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid";
 import theme from "./theme/theme";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AppNavbar from "./components/layout/AppNavbar";
+
+import RegistrarUsuario from './components/seguridad/RegistrarUsuario';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Grid container>
             <Switch>
                 <Route path="/" exact component={ListaInmobiliaria} />
+                <Route path="/auth/registrarUsuario" exact component={RegistrarUsuario} />
             </Switch>
         </Grid>
       </MuiThemeProvider>
---
+++ src/components/seguridad/RegistrarUsuario.js
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+  Container,
+  Typography,
+  Grid,
+  TextField,
+  Avatar,
+  Button,
+} from "@material-ui/core";
+import LockOutLineIcon from "@material-ui/icons/LockOutlined";
+
+const style = {
+  contents: {
+    display: "contents",
+  },
+  paper: {
+    marginTop: 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: 8,
+    backgroundColor: "#e53935",
+  },
+  form: {
+    width: "100%",
+    marginTop: 10,
+  },
+  center: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  submit:{
+      marginTop:30,
+      marginBottom: 20 
+  }
+};
+
+function RegistrarUsuario() {
+  return (
+    <div style={style.contents}>
+      <Container maxWidth="md">
+        <div style={style.paper}>
+          <Avatar style={style.avatar}>
+            <LockOutLineIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Registre su cuenta
+          </Typography>
+          <form style={style.form}>
+            <Grid container spacing={2} style={style.center}>
+              <Grid item md={6} xs={12}>
+                <TextField name="nombre" fullWidth label="Ingrese su nombre" />
+              </Grid>
+              <Grid item md={6} xs={12}>
+                <TextField
+                  name="apellido"
+                  fullWidth
+                  label="Ingrese sus apellidos"
+                />
+              </Grid>
+              <Grid item md={6} xs={12}>
+                <TextField name="email" fullWidth label="Ingrese su e-mail" />
+              </Grid>
+              <Grid item md={6} xs={12}>
+                <TextField
+                  name="password"
+                  fullWidth
+                  label="Ingrese su password"
+                />
+              </Grid>
+            </Grid>
+            <Grid container justify="center">
+              <Grid item md={6} xs={12}>
+                <Button type="submit" variant="contained" fullWidth size="large" color="primary" style={style.submit} >
+                    Registrar
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default RegistrarUsuario;
