ff76ad4fb41870b676e61b1c1092245c9cf81d6d
conradoRC <conrado27cym@gmail.com>
2020-07-07 10:49:21 -0500

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
-import ListaInmobiliaria from './components/ListaInmobiliaria';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import theme from './theme/theme';
+import ListaInmobiliaria from "./components/views/ListaInmobiliaria";
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid";
+import theme from "./theme/theme";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AppNavbar from "./components/layout/AppNavbar";
 
 function App() {
-  return <div>
-    <MuiThemeProvider theme={theme}>
-        <ListaInmobiliaria/>
-    </MuiThemeProvider>
-  </div>;
+  return (
+    <Router>
+      <MuiThemeProvider theme={theme}>
+        <AppNavbar/>
+        <Grid container>
+            <Switch>
+                <Route path="/" exact component={ListaInmobiliaria} />
+            </Switch>
+        </Grid>
+      </MuiThemeProvider>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/layout/AppNavbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+function AppNavbar (){
+        return (
+            <div>
+                <AppBar position="static" >
+                    <Toolbar />
+                </AppBar>
+            </div>
+        );
+}
+
+export default AppNavbar;
